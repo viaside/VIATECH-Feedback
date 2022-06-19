0dac20351426db4313155abbf740d1bd57270d0f
VIASIDE <VIASIDE@VIASIDE>
2022-06-19 21:25:54 +0300

    Добавьте файлы проекта.

---
+++ AnswerWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Npgsql;
+using System.Data;
+using System.Net.Mail;
+using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace VIATECH_Feedback
+{
+    /// <summary>
+    /// Логика взаимодействия для AnswerWindow.xaml
+    /// </summary>
+    public partial class AnswerWindow : Window
+    {
+        private string User_Name;
+        private string Send_Message;
+        private string User_EMail;
+
+        public AnswerWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadData(string us_name, string us_email)
+        {
+            User_Name = us_name;
+            User_EMail = us_email;
+            id.Content = User_EMail;
+            name.Content = User_Name;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Send_Message = Message_mail.Text;
+            MessageBox.Show($"Name - {User_Name} ,Email - {User_EMail}, Message - {Send_Message}");
+            Send_Email();
+        }
+
+        private void Send_Email()
+        {
+            // отправитель - устанавливаем адрес и отображаемое в письме имя
+            MailAddress from = new MailAddress("viaside15yo@yandex.ru", "ivan");
+            // кому отправляем
+            MailAddress to = new MailAddress(User_EMail);
+            // создаем объект сообщения
+            MailMessage m = new MailMessage(from, to);
+            // тема письма
+            m.Subject = "From VIAtech Feedback";
+            // текст письма
+            m.Body = Send_Message;
+            // письмо представляет код html
+            m.IsBodyHtml = true;
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.yandex.com", 587);
+            // логин и пароль
+            smtp.Credentials = new NetworkCredential("viaside15yo@yandex.ru", "zxxVanek1");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+using System.Data;
+
+namespace VIATECH_Feedback
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NpgsqlConnection npgSqlConnection = null;
+        private NpgsqlCommand npgSqlCommand = null;
+
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            npgSqlConnection = new NpgsqlConnection("Server=localhost;Port=5432;Database=VIAtech;User Id=postgres;Password=zxc;");
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string sql = (@"SELECT * FROM public.""Orders""");
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, npgSqlConnection);
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            zxc.ItemsSource = dt.DefaultView;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int id = zxc.SelectedIndex;
+            AnswerWindow answerWindow = new AnswerWindow();
+            answerWindow.Show();
+            string UserName = dt.Rows[id]["Name"].ToString();
+            string UserEmail = dt.Rows[id]["Email"].ToString();
+            answerWindow.LoadData(UserName, UserEmail);
+        }
+        
+    }
+}
